efcd3796bf9c1bb97aca112cdd1da47ebe8f6ee2
Joe White <joeswhite@users.noreply.github.com>
2013-11-19 16:32:05 -0500

    Create makebfgminer.sh
    this is the simple make file.
    chmod +x makebfgminer.sh
    ./makebfgminer.sh
    
    enjoy
    
    bfgminer is installed to ~/bfgminer
    
    this script removes any previous version of bfgminer. it does not install it as a command on your system

---
+++ makebfgminer.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#simple build script for bfg miner downloads all build tools and installs
+#This script is released as public domain software under the condition that you leave the developers contact info below
+#Developed by Joseph S. White joesfreicoinpool -at- gmail -dot- com http://pool.cr.rs
+#FRC (FreiCoin) donation address 1FRCJoeWXbYe47cmuW3do8VoqAr9HuWbpJ
+#BTC (BitCoin) donation address 1FRCJoeWXbYe47cmuW3do8VoqAr9HuWbpJ
+#Released on 10/29/13
+#
+#Version 0.002a
+rm -rfv ~/bfgminer/bfgminer
+cd  ~/
+sudo apt-get install git build-essential autoconf automake libtool pkg-config libcurl4-gnutls-dev libjansson-dev uthash-dev libncursesw5-dev libudev-dev libusb-1.0-0-dev libevent-dev libmicrohttpd-dev uthash-dev
+git clone https://github.com/luke-jr/bfgminer.git ~/bfgminer
+cd ~/bfgminer
+./autogen.sh
+./configure
+make
